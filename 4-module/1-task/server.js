d7ace69f1175d424d9c6dc6ca9b2e3537aa91e96
IvanVekshin <multipelder@gmail.com>
2020-02-03 23:38:05 +0300

    added solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,17 +1,28 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const pathIsNested = pathname.split('/').length > 1;
+  if (pathIsNested) {
+    res.statusCode = 400;
+    res.end('Sorry, nested paths are not supported');
+  }
 
   switch (req.method) {
     case 'GET':
+      const filepath = path.join(__dirname, 'files', pathname);
+      const readableStream = fs.createReadStream(filepath, { encoding: "utf8" });
+      readableStream.on('error', (err) => {
+        res.statusCode = 404;
+        res.end('Sorry, there is no such file on the server');
+      });
 
+      readableStream.pipe(res);
       break;
 
     default:
